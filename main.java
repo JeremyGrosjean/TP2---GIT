763525757207944f139cda9a058091a7790f2399
Jeremy GROSJEAN <jeremy.grosjean.icam@gmail.com>
2021-10-06 11:32:16 +0200

    Revert "DEV:Initialisation des points des maisons"
    This reverts commit c3ead8326c1881d9db3e52c50588bf9ef3f503de.

--- main.java
+++ main.java
@@ -14,10 +14,10 @@ public class main {
 			action = "";
 
 
-	        hellos.put("Gryffondor", "-50");
-	        hellos.put("Poufsouffle", "15");
-	        hellos.put("Serdaigle", "60");
-	        hellos.put("Serpentard", "10");
+	        hellos.put("Gryffondor", "0");
+	        hellos.put("Poufsouffle", "0");
+	        hellos.put("Serdaigle", "0");
+	        hellos.put("Serpentard", "0");
 
 
 	        Iterator<String> iterator;
